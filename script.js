5f0d5dd98f2d3c3b596c3814bae86d76a692baf3
unknown <camposvmiguelangel@gmail.com>
2021-04-22 13:14:29 +0200

    factorize code

--- script.js
+++ script.js
@@ -4,33 +4,38 @@ let instButtons = Array.from(document.querySelectorAll('#inst-buttons button'))
 let showResult = document.querySelector('#showResult')
 let note
 let inst
-let i = 0
 
-// Programa los botones de la tonalidad
-notesButtons.forEach(n => {
-    n.textContent = notes[i++]
-    n.onclick = function() {
-        notesButtons.forEach(b => b.classList.remove('selected'))
-        this.classList.add('selected')
-        note = notes.indexOf(this.textContent)
-        if (inst != undefined) transpose(note, inst)
-    }
-})
 
-// Programa los botones de instrumentos
-instButtons.forEach(n => {
-    n.onclick = function() {
-        instButtons.forEach(b => b.classList.remove('selected'))
-        this.classList.add('selected')
-        inst = notes.indexOf(this.querySelector('span').textContent)
-        if (note != undefined) transpose(note, inst)
-    }
-})
-
-// Realiza la transposición
-function transpose(note, inst) {
-    let result = note + inst
-    if (result > 11) result -= 12
-    showResult.classList.remove("hide")
-    showResult.innerText = notes[result]
+function programNotesButtons() {
+    notesButtons.forEach(n => {
+        n.onclick = function() {
+           switchSelectedButton(notesButtons, this)
+            note = notes.indexOf(this.textContent)
+            transpose(note, inst)
+        }
+    })
 }
+function programInstButtons() {
+    instButtons.forEach(n => {
+        n.onclick = function() {
+            switchSelectedButton(instButtons, this)
+            inst = notes.indexOf(this.querySelector('span').textContent)
+            transpose(note, inst)
+        }
+    })
+}
+function switchSelectedButton(buttons, button) {
+    buttons.forEach(button => button.classList.remove('selected'))
+    button.classList.add('selected')
+}
+function transpose(note, inst) {
+    if (inst != undefined && note != undefined) {
+        let result = note + inst
+        if (result > 11) result -= 12
+        showResult.classList.remove("hide")
+        showResult.innerText = notes[result]
+    }
+}
+
+programNotesButtons()
+programInstButtons()
