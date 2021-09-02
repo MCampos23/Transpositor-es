7cb4b17c31d5a0f987eba1423edfd5066a65836b
Miguel Campos <63472933+MCampos23@users.noreply.github.com>
2021-09-03 00:38:20 +0200

    refactoring function programInstButtons & programNotesButtons into one function

--- script.js
+++ script.js
@@ -1,50 +1,37 @@
 const notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
 
-let notesButtons = Array.from(
-  document.querySelectorAll("#notes-buttons button")
-);
+let notesButtons = Array.from(document.querySelectorAll("#notes-buttons button"));
 let instButtons = Array.from(document.querySelectorAll("#inst-buttons button"));
-let showResult = document.querySelector("#showResult");
 
+let showResult = document.querySelector("#showResult");
 
 let note;
 let inst;
 
-function programNotesButtons() {
-  notesButtons.forEach((noteButton) => {
-    noteButton.onclick = function () {
-      switchSelectedButton(notesButtons, this);
-      note = notes.indexOf(this.id);
-      transpose(note, inst);
-    };
-  });
-}
-function programInstButtons() {
-  instButtons.forEach((instButton) => {
-    instButton.onclick = function () {
-      switchSelectedButton(instButtons, this);
-      inst = notes.indexOf(this.id);
+function programButtons(buttons) {
+  buttons.forEach((button) => {
+    button.onclick = function () {
+      applyBorderToSelectedButton(buttons, this);
+      buttons === notesButtons ?  note = notes.indexOf(this.id) :  inst = notes.indexOf(this.id);
       transpose(note, inst);
     };
   });
 }
 
-function switchSelectedButton(buttons, button) {
+function applyBorderToSelectedButton(buttons, button) {
   buttons.forEach((button) => button.classList.remove("selected"));
   button.classList.add("selected");
 }
+
 function transpose(note, inst) {
   if (inst != undefined && note != undefined) {
     let result = note + inst;
     if (result > 11) result -= 12;
     showResult.classList.remove("hide");
-    if (anglo=true)
-    showResult.innerText = notes[result]
-    else{
-        showResult.innerText = latinNotes[result]
-    }
+    showResult.innerText = notes[result];
   }
 }
 
-programNotesButtons();
-programInstButtons();
+programButtons(notesButtons)
+programButtons(instButtons)
+
